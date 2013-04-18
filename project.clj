0c5d875d9b6cdc2ae097c4267f4eb2e198b8b0aa
tlipski <aston.8>
2013-04-18 19:28:50 +0200

    update version to 0.9.0

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 (defproject ganelon-tutorial "0.9-SNAPSHOT"
   :description "Ganelon tutorial"
   :url "http://ganelon.tomeklipski.com"
-  :dependencies [[ganelon "0.9-SNAPSHOT"]
+  :dependencies [[ganelon "0.9.0"]
                  [congomongo "0.4.1"]
                  [crypto-random "1.1.0"]]
   :license {:name "Eclipse Public License - v 1.0"
