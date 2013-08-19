4a123d8d61a150679b8cabcca25f77d4aac81f8d
Tomek Lipski <tomek.lipski@gmail.com>
2013-08-19 21:11:42 +0200

    moved run.clj to scripts/ from tests/

--- script/run.clj
+++ script/run.clj
@@ -6,7 +6,7 @@
 ;; agreeing to be bound by the terms of this license.  You must not
 ;; remove this notice, or any other, from this software.
 
-(ns ganelon.tutorial.run
+(ns run
   (:require [ganelon.tutorial]))
 
 (ganelon.tutorial/-main)
